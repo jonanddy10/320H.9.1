1f1fb6441edef3ff151f0264aa20f33b51528f85
jonanddy10 <jonsairmail@icloud.com>
2024-03-17 10:22:41 -0700

    tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import List from './components/list'
 
 function App() {
-  const [count, setCount] = useState(10)
-  const [phrase, setPhrase] = useState("This is the start")
-
-  function decrementCount() {
-    if(count <= 1){
-      statusOver()
-    }
-    setCount(prevCount => prevCount -1)
-  }
-  function incrementCount() {
-    if(count > 1){
-      statusOK()
-    }
-    setCount(prevCount => prevCount + 1)
-  }
-  function statusOK() {
-    setPhrase("Keep going!")
-  }
-  function statusOver() {
-    setPhrase("The end.")
-  }
-  function justPractice(){
-    console.log("Peakaboo")
-  }
 
   return (
     <>
-      <h1 className='header'>to-do list</h1>
-
-      <button onClick={decrementCount}>-</button>
-      <span>{count}</span>
-      <button onClick={incrementCount}>+</button>
-      <br/>
-      <span>{phrase}</span>
-      <br/>
-      <button onClick={justPractice}>...</button>
+    <List/>
     </>
   )
 }
--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,10 +1,93 @@
-// This list component contains the list items and stretches as the list fills up
-import React from 'react'
+import { useState } from 'react'
 
-function list() {
+function List() {
+  const [task, setTask] = useState(["Go on a run", "Eat breakfast", "Drop off keys"])
+  const [newTask, setNewTask] = useState("")
+  
+  function onChange(e){
+    setNewTask(e.target.value)
+  }
+
+  function addTask(){
+    // prevent an empty string from being added
+    if(newTask.trim() !== ""){
+    setTask(prevTask => [...prevTask, newTask])
+    setNewTask("")
+    }
+  }
+
+  function deleteTask(index){
+    const update = task.filter((element, i) => i !== index)
+    setTask(update)
+  }
+
+  function moveUp(index){
+    if(index > 0){
+      const update = [...task];
+      // switch positions
+      [update[index], update[index - 1]] = 
+      [update[index - 1], update[index]]
+      setTask(update)
+    }
+  }
+
+  function moveDown(index){
+    if(index < task.length - 1){
+      const update = [...task];
+      // switch positions
+      [update[index], update[index + 1]] = 
+      [update[index + 1], update[index]]
+      setTask(update)
+    }
+  }
+
   return (
-    <div>list</div>
+    <div className="to-do-list">
+      <h1 className='header'>todo list</h1>
+
+      <div>
+        <input
+        type="text"
+        placeholder="New task"
+        value={newTask}
+        onChange={onChange}/>
+
+        <button 
+          className="add-button" 
+          onClick={addTask}>
+          add
+        </button>
+      </div>
+
+      <ol>
+        {task.map((task, index) =>
+  
+        <li key={index}>
+          <span className="text">{task}</span>
+          
+          <button 
+            className="delete-button"
+            onClick={() => deleteTask(index)}>
+            Delete
+          </button>
+
+          <button 
+            className="move-button"
+            onClick={() => moveUp(index)}>
+            Move up
+          </button>
+          
+          <button 
+            className="move-button"
+            onClick={() => moveDown(index)}>
+            Move down
+          </button>
+        
+        </li>
+        )}
+      </ol>
+    </div>
   )
 }
 
-export default list
+export default List
